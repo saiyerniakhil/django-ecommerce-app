69368cb63eb085cf33196eb4791d4a3fde5a23a0
Sai Yerni Akhil Madabattula <makhil123@yahoo.in>
2023-08-26 17:22:31 -0500

    add order model, serializer, views and urls

--- ecom/api/urls.py
+++ ecom/api/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', home, name='api.home'),
     path('category/', include('api.category.urls')),
     path('product/', include('api.product.urls')),
-    path('user/', include('api.user.urls'))
+    path('user/', include('api.user.urls')),
+    path('order/', include('api.order.urls')),
+    path('api-auth-token/', views.obtain_auth_token, name='api_auth_token')
 ]
